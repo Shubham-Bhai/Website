408d05913c2ef0f03a51b2dc9d47e9332f152fd7
ClutchGodShubham07 <87593401+ClutchGodShubham07@users.noreply.github.com>
2024-04-18 16:59:24 +0530

    script.js

--- script.js
+++ script.js
@@ -16,3 +16,39 @@ document.addEventListener("DOMContentLoaded", function() {
       });
     });
   });
+
+  const texts = [
+    "Currently, I'm a student in the 11th grade.",
+    "Anime holds a special place in my heart.",
+    "Despite my skill level, I enjoy playing chess."
+  ];
+  
+  const delay = 50; // Change this to set the interval (in milliseconds) between typing each character
+  const textElement = document.getElementById('text');
+  const cursorElement = document.createElement('span');
+  cursorElement.className = 'cursor';
+  cursorElement.innerText = '|';
+  textElement.appendChild(cursorElement);
+  
+  let textIndex = 0;
+  let charIndex = 0;
+  
+  function type() {
+    const text = texts[textIndex];
+    if (charIndex <= text.length) {
+      const displayText = text.substring(0, charIndex);
+      textElement.innerText = displayText;
+      textElement.appendChild(cursorElement);
+      charIndex++;
+      setTimeout(type, delay);
+    } else {
+      // Move to the next text after a delay
+      textIndex = (textIndex + 1) % texts.length;
+      charIndex = 0;
+      setTimeout(type, 2000); // Change the delay between texts as needed
+    }
+  }
+  
+  // Start the typing process
+  type();
+  
